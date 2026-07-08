34c2305307dd372c07b80749883909abfb2ee24f
oalhaiija-volume <o.alhaija@gmail.com>
2026-07-08 17:41:12 +0300

    Pad Alumex web app icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,6 @@ export const metadata: Metadata = {
       { url: "/icons/alumex-symbol-32.png", sizes: "32x32", type: "image/png" },
       { url: "/icons/alumex-symbol-192.png", sizes: "192x192", type: "image/png" },
       { url: "/icons/alumex-symbol-512.png", sizes: "512x512", type: "image/png" },
-      { url: "/logos/AlumexIcon.svg", type: "image/svg+xml" },
     ],
     apple: [
       { url: "/icons/alumex-symbol-180.png", sizes: "180x180", type: "image/png" },
